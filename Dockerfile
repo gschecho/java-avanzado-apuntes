97b16b698deafb5e436a7d532802b348db5f827b
Gon <gschecho@gmail.com>
2025-01-20 17:27:27 +0100

    Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+#COMANDOS A EJECUTAR
+
+## docker build -t nombre-imagen .
+## docker run -d -p 8080:8080 nombre-app
+
+
+# Stage 1: Build the application
+FROM eclipse-temurin:8-jdk AS builder
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the application code
+COPY . .
+
+# Given permissions to mvnw
+RUN chmod +x mvnw
+
+# Build the application (requires Maven or Gradle)
+RUN ./mvnw clean package -DskipTests
+
+# Stage 2: Run the application
+FROM eclipse-temurin:8-jre
+
+# Set the working directory
+WORKDIR /app
+
+# Copy the JAR file from the builder stage
+COPY --from=builder /app/target/*.jar app.jar
+
+# Expose the port the app will run on
+EXPOSE 8080
+
+# Command to run the application
+ENTRYPOINT ["java", "-jar", "app.jar"]
--- src/test/java/junit/JunitTest.java
+++ src/test/java/junit/JunitTest.java
@@ -1,12 +1,13 @@
 package junit;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+import org.junit.internal.runners.statements.Fail;
+import org.junit.rules.ExpectedException;
 
 import java.util.Arrays;
 import java.util.List;
 
+import static junit.framework.TestCase.fail;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -15,6 +16,10 @@ public class JunitTest {
     private List<String> palabras;
 
 
+    @BeforeClass
+    public static void inicialization(){
+        System.out.println("Solo ocurre una vez antes de que se inicie la clase");
+    }
     @Before
     public void setUp(){
 
@@ -23,9 +28,10 @@ public class JunitTest {
     }
 
 
+    @Test @Ignore
     public void  hasSixStringsAssert(){
 
-
+        Assert.assertEquals("debe ser un numero 6",6, palabras.size());
     }
 
 
@@ -33,6 +39,37 @@ public class JunitTest {
     @Test
     public void hasSixStrings(){
         assertThat(palabras.size(), is(6));
+    }
+
+
+    // Exceptions
+    // Old style junit 3
+    public void nullListThrowsNPEANTES(){
+        palabras = null;
+        try{
+            palabras.add("test");
+            fail("Debería haber tirado una excepcion");
+        }catch(NullPointerException ex){
+        Assert.assertNull(palabras);}
+    }
+
+//New stile junit 4
+    @Test(expected = NullPointerException.class)
+    public void nullListThrowsNPE(){
+        palabras= null;
+        palabras.add("test");
+    }
+
+    // Podemos decirle que tipo de excepcion se espera
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void nullListThrowsNPEUsingRule(){
+        String[] stringArray = palabras.toArray(new String[0]);
+        thrown.expect(ArrayIndexOutOfBoundsException.class);
+        thrown.expectMessage("7");
+        System.out.println(stringArray[7]);
     }
 
 
